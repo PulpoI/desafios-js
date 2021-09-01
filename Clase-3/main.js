c86fd7ca3bf1c38931c1a3f478dae2ab4222e679
Pulpoi <pablo37942814@gmail.com>
2021-09-01 00:58:58 -0300

    desafio 3 terminado

---
+++ Clase-3/main.js
@@ -0,0 +1,98 @@
+//For
+/*
+for(let i = 1; i<= 20; i++){
+    
+    console.log(i);
+}
+*/
+/*
+for(let i = 5; i < 100; i++) {
+    if(i >= 50 && i <= 60 ){
+        continue;
+    }else if(i == 70){
+        break;
+    }
+    console.log(i)
+}
+*/
+
+//Calculadora
+/* 
+let numero = parseInt(prompt("Ingresar Numero"));
+for (let i = 1; i <10; i++) {
+    let resultado = numero * i;
+    console.log(numero + " X " + i + " = " + resultado);
+}
+*/
+
+// WHILE
+/*
+let edadMirta = prompt("Ingresar la edad de Mirta");
+while (edadMirta != 94) {
+    alert("Pifiaste!");
+    edadMirta = prompt("Volve a interlo, Mirta tiene?");
+    
+}
+*/    
+/*
+// SWITCH 
+let carrera = prompt("Ingresa tu carrera")
+while (carrera != "NINGUNA") {
+    switch (carrera) {
+        case "DISEÑO":
+            alert("Te inscribiste en Diseño");
+            break;
+        case "INFORMATICA":
+            alert("Te inscribiste en Informatica"); 
+            
+            break;
+        default:
+            alert("Indica una carrera por favor");
+            break;
+    }
+    carrera = prompt("Ingresa carrera");
+}
+*/
+
+
+
+
+// DESAFIO
+/*
+let numeroUnaCifra = parseInt(prompt("Ingresa un numero de una cifra para ver la tabla en la consola"));
+for (let i = 1; i < 10; i++){
+    let resultado = numeroUnaCifra * i; 
+    console.log(numeroUnaCifra + " X " + i +  " es igual a: " + resultado)
+}
+
+
+let prenda = prompt("Ingrese la prenda que desea agregar al carrito");
+while (prenda != "ESC") {
+    console.log("El usuario agregó " + prenda + " al carrito");
+    prenda = prompt("Ingrese otra prenda o ingrese ESC para terminar");
+}
+*/
+
+
+let mensaje = "Hola";
+let numeroHola = parseInt(prompt("Ingresa un numero del 1 al 3"));
+while (numeroHola != ""){
+    switch (numeroHola) {
+        case 1:
+            alert("Hola");
+            numeroHola = ""
+            break;
+        case 2:
+            alert("Hola Hola");
+            numeroHola = ""
+            break;  
+        case 3: 
+            alert("Hola Hola Hola");
+            numeroHola = ""
+            break;
+        default:
+            alert("El numero no es valido")
+            numeroHola = parseInt(prompt("Proba con un numero del 1 al 3"));
+            break;
+    }
+}    
